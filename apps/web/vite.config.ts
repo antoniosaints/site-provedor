c6b106ee5b6eb254a8f348abd745d5dbf40e72c8
Antonio Santos <68619881+antoniosaints@users.noreply.github.com>
2026-05-20 10:26:42 -0300

    feat(ui) ajuste de UI

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     host: '0.0.0.0',
     port: 5173,
     allowedHosts: [
-      '.meganetma.com.br',
+      'meganetma.com.br',
       'www.meganetma.com.br'
     ],
     proxy: {
@@ -29,7 +29,7 @@ export default defineConfig({
     host: '0.0.0.0',
     port: 5173,
     allowedHosts: [
-      '.meganetma.com.br',
+      'meganetma.com.br',
       'www.meganetma.com.br'
     ]
   }

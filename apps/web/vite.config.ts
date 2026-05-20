82d1ee61a807076ab24fa6505d396b9699398815
Antonio Santos <68619881+antoniosaints@users.noreply.github.com>
2026-05-20 08:33:15 -0300

    feat(melhorias)

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -3,11 +3,26 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+
   server: {
+    host: '0.0.0.0',
     port: 5173,
+    allowedHosts: [
+      'meganetma.com.br',
+      'www.meganetma.com.br'
+    ],
     proxy: {
       '/api': 'http://localhost:3333',
       '/uploads': 'http://localhost:3333'
     }
+  },
+
+  preview: {
+    host: '0.0.0.0',
+    port: 5173,
+    allowedHosts: [
+      '.meganetma.com.br',
+      'www.meganetma.com.br'
+    ]
   }
 });
